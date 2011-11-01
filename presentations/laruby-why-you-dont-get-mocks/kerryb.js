75dff60ffaba9742ffbde1d6e569b2be48efb4c7
Neil Middleton <neil.middleton@gmail.com>
2011-11-01 21:59:26 +0000

    Remove dead files

--- presentations/laruby-why-you-dont-get-mocks/kerryb.js
+++
@@ -1,8 +0,0 @@
-/*jslint white: true, evil: true,browser: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: true */
-/*global window: false, jQuery: false */
-"use strict";
-
-$(document).ready(function(){
-  SyntaxHighlighter.defaults["gutter"] = false;
-  SyntaxHighlighter.defaults["toolbar"] = false;
-});
